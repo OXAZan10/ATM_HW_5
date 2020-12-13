74920a138cba728afadab73c73d0f2db4b4c6479
Aksana Zanko <Aksana_Zanko@epam.com>
2020-12-13 15:09:36 +0300

    mochawesome

--- test/specs/add.spec.js
+++ test/specs/add.spec.js
@@ -1,13 +1,30 @@
 const Calculator = require('../../app/calculator');
 const {expect} = require('chai');
 
-describe('add positive scenarios', function(){
+describe('scenarios for 2 arguments', function(){
     let result;
     beforeEach(function(){
         result = new Calculator();
     });
-    it('Should return summ', function(){
+    afterEach(function(){
+        result = null;
+    });
+    it('Should return result =2 for 1+1', function(){
         expect(result.add(1,1)).to.be.equal(2);
-
+    });
+    it('Should return result =1 for 1+0', function(){
+        expect(result.add(1,0)).to.be.equal(1);
+    });
+    it('Should return result =0 for -1+1', function(){
+        expect(result.add(-1,1)).to.be.equal(0);
+    });
+    it('Should return result =0 for 1*0', function(){
+        expect(result.multiply(1,0)).to.be.equal(0);
+    });
+    it('Should return result =1 for 1*1', function(){
+        expect(result.multiply(1,1)).to.be.equal(1);
+    });
+    it('Should return result =1 for -1*1', function(){
+        expect(result.multiply(-1,1)).to.be.equal(-1);
     });
 });
